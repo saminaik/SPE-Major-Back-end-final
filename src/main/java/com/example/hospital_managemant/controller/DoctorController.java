6ade84493eed3eff40072bd9a6beb05e447bda49
samiullahnaikoo <sami.naikoo@iiitb.ac.in>
2023-05-11 11:19:43 +0530

    may 11 11:19

--- src/main/java/com/example/hospital_managemant/controller/DoctorController.java
+++ src/main/java/com/example/hospital_managemant/controller/DoctorController.java
@@ -8,12 +8,16 @@ import com.example.hospital_managemant.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 @RequestMapping("api/doctor")
 @RestController
 public class DoctorController {
+    private static final Logger logger = LoggerFactory.getLogger(DoctorController.class);
+
     @Autowired
     private final DoctorService doctorService;
 
@@ -36,23 +40,31 @@ public class DoctorController {
     {
         List<Appointment> appointments = doctorService.getAllAppointments(id);
         System.out.println(appointments);
-        if (appointments.size()==0)
-            return null;
+        if (appointments.size()==0){
+            logger.warn("No appointments found.");
+            return null;}
+        logger.info("Retrieved  appointments.", appointments.size());
         return appointments;
     }
     @PostMapping("/treatment")
     public ResponseEntity<?> setTreatment(@RequestBody Treatement treat){
+        logger.info("Received request to set treatment for patient");
+
         return doctorService.setTreatment(treat);
     }
     @PostMapping("/pres")
     public ResponseEntity<?> setPrescription(@RequestBody Prescription prescription)
     {
+        logger.info("Received request to set prescription for Appointment with id {}.", prescription.getAppointmentId());
+
         return doctorService.setPriscription(prescription);
     }
 
     @PostMapping("appoinmentApprove/{id}/{ISAPPROVED}")
     public ResponseEntity<?> approveAppoinment(@PathVariable Long id,@PathVariable String ISAPPROVED){
         System.out.println(ISAPPROVED);
+        logger.info("Received request to approve appointment with id {}.", id);
+
         return doctorService.approveAppoinment(id,ISAPPROVED);
     }
 }
